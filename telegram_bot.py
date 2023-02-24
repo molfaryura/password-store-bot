8b9fa54bfa3000568d2869867f6cbdb224d6549e
molfaryura <oleksandrr1996@protonmail.com>
2023-02-24 09:33:07 +0200

    Added asyncio task and a cancel command
    Asyncio task deletes the password in the chat
    after a certain period of time. The cancel command
    is used for finishing all states if the user do not
    want to continue.

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,9 @@ from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import Text
+
+import asyncio
 
 import os
 
@@ -32,6 +35,15 @@ async def start(message:types.Message):
     
     await bot.send_message(message.chat.id, info, parse_mode='html')
 
+@dp.message_handler(commands=['cancel'], state="*")
+@dp.message_handler(Text(equals="cancel", ignore_case=True), state="*")
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.answer("Done!")
+
 
 @dp.message_handler(commands=['add'], state=None)
 async def add(message:types.Message):
@@ -39,18 +51,22 @@ async def add(message:types.Message):
     await bot.send_message(message.chat.id, 'Hi, what is this account?')
 
 @dp.message_handler(state=Form.account)
-async def account(message:types.Message, state: FSMContext):
+async def account(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['account'] = message.text
     await Form.next()
-    await  message.answer("Type your password: ")
+    await message.answer("Type your password: ")
+
 
 @dp.message_handler(state=Form.password)
 async def pwd(message:types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['password'] = message.text
     await Form.next()
+    #task = asyncio.create_task(asyncio.sleep(5))
     await  message.answer("Type your secret word: ")
+    #await task
+    #await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
 
 
 @dp.message_handler(state=Form.secret_word)
