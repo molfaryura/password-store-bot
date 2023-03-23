43bf47ef3d9a94e5e842122f6ca05de83e7f097f
molfaryura <oleksandrr1996@protonmail.com>
2023-03-23 18:53:08 +0200

    Fixed code style issues

--- key.py
+++ key.py
@@ -1,11 +1,13 @@
+"""Key for encryption and decryption"""
+
 import secrets
 
 import hashlib
 
 from dotenv import set_key
 
-hash = hashlib.sha256()
-hash.update(secrets.token_hex(64).encode('utf8'))
-hash = hash.hexdigest()
+HASH = hashlib.sha256()
+HASH.update(secrets.token_hex(64).encode('utf8'))
+HASH = hash.hexdigest()
 
-set_key('.env', 'SECRET_KEY', hash) 
+set_key('.env', 'SECRET_KEY', HASH)
